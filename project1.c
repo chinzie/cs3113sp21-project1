4ef0b19ae9b5dcec6893e0bac3f38e3420700cec
Colin Hinzie <hinzieoak73@gmail.com>
2021-04-06 02:46:26 +0000

    changed for not just stdin

--- project1.c
+++ project1.c
@@ -6,23 +6,60 @@
 #include <sys/wait.h>
 #include <stdint.h>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	unsigned char *buf = (char *) malloc (1000000 *  sizeof(char));
+	char *buf = (char *) malloc (1000000 *  sizeof(char));
 	int count = 0;
 	int index = 0;
-	double P; //Processes available to run instructions (always 1)
-        double p; //Number of threads (amount of processes)
-        double N; //Number of instructions (number of instruction lines)
+	int P; //Processes available to run instructions (always 1)
+        int p; //Number of threads (amount of processes)
+        int N; //Number of instructions (number of instruction lines)
 	char temp[100000];
 	char pid[100000];
 	char burst[100000];
 	if (argc > 1)
 	{
-		printf("not text");
+		FILE *file_handle;
+		file_handle = fopen(argv[1], "r");
+		
+        	while (fgets(buf, 1, file_handle) != NULL)
+        	{
+                	buf[index] = *buf;
+                	unsigned char c = buf[index];
+                	if (buf[index] == '\n'|| buf[index] == ' ')
+                	{
+                        	//printf("newline");
+                        	continue;
+                	}
+                	if (count == 0)
+                	{
+                        	//printf("P is set\n");
+                        	P = c - '0';
+
+                	}
+                	if (count == 1)
+                	{
+
+                        	//printf("p is set\n");
+                        	p = c - '0';
+                	}
+                	if (count == 2)
+                	{	
+
+                        	//printf("N is set\n");
+                        	N = c - '0';
+                	}
+                	if (count > 2)//start reading instructions
+                	{
+                        	temp[index] = c;
+                        	index++;
+                	}
+        	        count++;
+	        }
+
+
 	}
 	int n;
-	if (argc > 1)
 	while ((n = (read(0, buf, 1))) > 0)
 	{
 		buf[index] = *buf;
@@ -122,7 +159,7 @@ int main(int argc, char **argv)
 	//printf("avg response time: %.2f\n", avgWaits);
 
 
-	double voluntarySwitch = p;
+	int voluntarySwitch = p;
 	//printf("voluntary: %d\n", voluntarySwitch);
 	//printf("turnaround time: %.2f\n", turnAround);
 	//calculate response time
@@ -130,7 +167,7 @@ int main(int argc, char **argv)
 	double waiting = turnAround - avgBurst;
 	//printf("average wait time: %.2f\n", waiting);
 
-	double nonVoluntarySwitch;
+	int nonVoluntarySwitch;
 
 	int counter = 0;
 	for (int i = 0; i < dep; i++)
@@ -168,8 +205,8 @@ int main(int argc, char **argv)
 
 	double cpuUtilization = 100.00; //util always 100 for this instance as P = 1
 
-	printf("%f\n", voluntarySwitch);
-	printf("%f\n", nonVoluntarySwitch);
+	printf("%d\n", voluntarySwitch);
+	printf("%d\n", nonVoluntarySwitch);
 	printf("%.2lf\n", cpuUtilization);
 	printf("%.2lf\n", throughput);
 	printf("%.2lf\n", turnAround);
